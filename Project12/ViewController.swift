b5949afc8fc0d9f1ce9a2000cd1fb02a339bfc66
FrankSolleveld <fsolleveld@outlook.com>
2021-03-14 17:54:47 +0100

    core basics of user defaults

--- Project12/ViewController.swift
+++ Project12/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let defaults = UserDefaults.standard
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "ArrayTest")
+        let dict = ["Name": "Frank", "Country": "NL"]
+        defaults.set(dict, forKey: "SavedDict")
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBoolean = defaults.bool(forKey: "UseFaceID")
+        let savedArray = defaults.object(forKey: "ArrayTest") as? [String] ?? [String]()
+        let savedDict = defaults.object(forKey: "SavedDict") as? [String: String] ?? [String: String]()
     }
 
 
